c2efdcb87b29d4bbe0f2937320e945338bc4e844
Chris <compsciguy777@gmail.com>
2020-10-16 07:30:31 -0400

    More Improvements

--- src/App.js
+++ src/App.js
@@ -28,6 +28,11 @@ export default function App() {
   return (
     <div className="App">
       <h1>To Do List</h1>
+      <div className="inputGroup">
+        <label for="TodoInput">Your Next To Do: </label>
+        <input id="TodoInput" />
+        <button type="submit">Submit</button>
+      </div>
       <ul>
         {todo.map((todo) => (
           <li className="listItem">
