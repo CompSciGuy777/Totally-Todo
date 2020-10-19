a6b92cf9405651f06687f8bee2e2e662a9a5ed46
Chris <compsciguy777@gmail.com>
2020-10-19 08:02:08 -0400

    minor updates

--- src/components/InputGroup.js
+++ src/components/InputGroup.js
@@ -5,14 +5,7 @@ const InputGroup = () => {
     <div className="inputGroup">
       <label htmlFor="TodoInput">Your Next To Do: </label>
       <input id="TodoInput" />
-      <button
-        type="submit"
-        onClick={() => {
-          console.log('This Button was Clicked');
-        }}
-      >
-        Submit
-      </button>
+      <button type="submit">Submit</button>
     </div>
   );
 };
